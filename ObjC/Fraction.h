8a22281b6c1f8993c051426e06b3ee862829d4e7
Yun Ji Gwak <jiesiana2@gmail.com>
2016-05-24 16:46:19 +0900

    Fraction 클래스 분리

---
+++ ObjC/Fraction.h
@@ -0,0 +1,20 @@
+//
+//  Fraction.h
+//  ObjC
+//
+//  Created by MF839-015 on 2016. 5. 24..
+//  Copyright © 2016년 Sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// 인터페이스
+@interface Fraction : NSObject
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+-(int) numerator;
+-(int) denominator;
+@end
+
+// implementation 부분
---
+++ ObjC/Fraction.m
@@ -0,0 +1,41 @@
+//
+//  Fraction.m
+//  ObjC
+//
+//  Created by MF839-015 on 2016. 5. 24..
+//  Copyright © 2016년 Sample. All rights reserved.
+//
+
+#import "Fraction.h"
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int)n
+{
+    numerator = n;
+}
+
+-(void) setDenominator:(int)d
+{
+    denominator = d;
+}
+
+-(int) numerator
+{
+    return numerator;
+}
+
+-(int) denominator
+{
+    return denominator;
+}
+
+@end
